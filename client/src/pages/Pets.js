b2dd93968bbed82995e5ed4492227f001b515413
yuri <7777kara@gmail.com>
2023-09-05 20:08:24 +0200

    shows the list of pets

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,9 +5,32 @@ import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const ALL_PETS = gql`
+    query AllPets {
+        pets {
+            name
+            id
+            type
+            img
+        }
+    }
+`
+
 export default function Pets () {
   const [modal, setModal] = useState(false)
+  const {data, loading, error} = useQuery(ALL_PETS);
 
+
+  if (loading) {
+    return <Loader />
+  }
+
+  if (error) {
+    return <p>{error}</p>
+  }
+
+  console.log('1', data);
+  console.log('l', loading);
 
   const onSubmit = input => {
     setModal(false)
@@ -31,7 +54,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
   )
